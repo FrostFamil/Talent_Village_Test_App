e157cc79c67705ef915e96658823ad7918668b1c
Famil <frostgamingrespawn@gmail.com>
2021-02-19 01:28:40 +0100

    Added story component to the Home Screen

--- test/src/screens/HomeScreen.js
+++ test/src/screens/HomeScreen.js
@@ -1,14 +1,44 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, ScrollView} from 'react-native';
 import styles from '../styles/styles';
+import StoryComponent from '../components/StoryComponent';
+import list_response from '../mockData/list_response.json';
 
 
 
 const HomeScreen = () => {
 
+    const [response, setResponse] = useState({});
+
+    useEffect(() => {
+        //Here usually will be custom fetch hook to handle the api requests.
+
+        setResponse(list_response);
+    }, [])
+
     return (
         <View style={styles.homeScreenMainView}>
-            <Text>Test</Text>
+            <View style={styles.homeScreenTextView}>
+                <Text style={styles.homeScreenText}>
+                    Welcome to the Community
+                </Text>
+            </View>
+
+            <View style={{marginTop: 20}}>
+                <ScrollView
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.homeScreenStoryScrollView}
+                >
+                    {list_response.data.new_users.map(data => (
+                        <StoryComponent
+                            key={data.id} 
+                            url={data.image_url}
+                            isOnline={data.isOnline}
+                        />
+                    ))}
+                </ScrollView>
+            </View>
         </View>
     )
 }
