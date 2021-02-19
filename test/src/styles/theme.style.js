0bfc826dc6bed8ebfe4ab81fc259713cd9331e68
Famil <frostgamingrespawn@gmail.com>
2021-02-19 01:29:08 +0100

    styles for story component added to story file

--- test/src/styles/theme.style.js
+++ test/src/styles/theme.style.js
@@ -2,5 +2,8 @@ export default {
   // Font
 
   // Colors
-  PRIMARY_COLOR: '#F1F1F0'
+  PRIMARY_COLOR: '#F1F1F0',
+  DARK_GOLD_COLOR: '#AA6C39',
+  ONLINE_COLOR: '#00CC00',
+  OFFLINE_COLOR: '#989898'
 };
