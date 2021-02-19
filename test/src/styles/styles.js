bd45bfa65d66b5cba3d4c086eeddaf372f409421
Famil <frostgamingrespawn@gmail.com>
2021-02-19 12:11:54 +0100

    styles added for card component

--- test/src/styles/styles.js
+++ test/src/styles/styles.js
@@ -55,6 +55,81 @@ const styles = {
     borderRadius: 40,
     left: 77,
     bottom: 28
+  },
+
+  //Card Component Styles
+  cardComponentMainView: {
+    backgroundColor: themeStyle.WHITE,
+    borderRadius: 20,
+    marginTop: 10
+  },
+
+  cardComponentDescriptionTextView: {
+    marginHorizontal: 7
+  },
+
+  cardComponentDescriptionText: {
+    color: themeStyle.LIGHT_GREY
+  },
+
+
+  //Image Icons Component Styles
+  imageIconComponentMainView: {
+    flex: 1,  
+    margin: 10
+  },
+
+  imageIconComponentImage: {
+    width: '100%', 
+    height: 200, 
+    borderRadius: 10
+  },
+
+  imageIconComponentIconsView: {
+    flexDirection: 'row', 
+    justifyContent: 'flex-start', 
+    marginTop: 10
+  },
+
+  imageIconComponentTextView: {
+    marginVertical: 25, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+
+  imageIconComponentText: {
+    fontWeight: '500'
+  },
+
+
+  //Logo Brand Date Component Styles
+  logoBrandDateComponentMainView: {
+    flexDirection: 'row'
+  },
+
+  logoBrandDateComponentStoryAndTextsView: {
+    flexDirection: 'row'
+  },
+
+  logoBrandDateComponentBrandNameAndDateView: {
+    justifyContent: 'center'
+  },
+
+  logoBrandDateComponentBrandNameText: {
+    fontWeight: '500', 
+    fontSize: 16
+  },
+
+  logoBrandDateComponentDateText: {
+    opacity: 0.6, 
+    fontSize: 10
+  },
+
+  logoBrandDateComponentIconView: {
+    flex: 1, 
+    alignItems: 'flex-end', 
+    right: 20, 
+    justifyContent: 'center'
   }
 };
 
